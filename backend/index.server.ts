6f2c3653a02a1c52b5cfa00bf43246f23a67e4b2
Mohit Soni <mohitsoni9731@gmail.com>
2024-09-08 17:18:24 +0530

    Done some changes in index.server.ts in order to cope with CORS error

--- backend/index.server.ts
+++ backend/index.server.ts
@@ -1,9 +1,7 @@
 import { dbConnect } from './database/db.database'
-
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
-
 import authRoute from './routes/auth.routes'
 import messageRoute from './routes/message.routes'
 import userRoute from './routes/getUsers.routes'
@@ -15,17 +13,25 @@ dotenv.config()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({
-   origin: ['http://localhost:3000', 'https://social-messaging-application.netlify.app'],
-   methods: ['GET', 'POST'],
-   allowedHeaders: ['Content-Type', 'Authorization'] 
-}));
 
+const corsOptions = {
+   origin: ['http://localhost:3000', 'https://social-messaging-application.netlify.app'],
+   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+   allowedHeaders: ['Content-Type', 'Authorization'],
+   credentials: true
+};
+
+app.use(cors(corsOptions))
 app.use('/api/auth', authRoute)
 app.use('/api/messages', messageRoute)
 app.use('/api/users', userRoute)
 
-app.listen(process.env.Port || 8000, () => {
-   dbConnect()
-   console.log('It is listening')
+app.listen(process.env.PORT || 8000, async() => {
+   try {
+      await dbConnect()
+      console.log(`Server is running on port ${PORT}`);
+    } catch (error) {
+      console.error('Failed to connect to the database', error);
+      process.exit(1); // Exit the process with failure
+    }
 })
