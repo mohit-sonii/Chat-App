25738f9f6a5b26650da9dd695e685a796267054d
Mohit Soni <mohitsoni9731@gmail.com>
2024-09-18 08:17:11 +0530

    trying #3 to solve the Axios 404 not found in login and register This time I have added CORSin socket.ts and removed from index.server.ts

--- backend/socket/socket.ts
+++ backend/socket/socket.ts
@@ -6,9 +6,11 @@ const app = express()
 const server = http.createServer(app)
 const io = new Server(server, {
    cors: {
-      origin: ['http://localhost:5173', 'https://social-messaging-application.netlify.app/'],
+      origin: 'https://social-messaging-application.netlify.app',
       methods: ['GET', 'POST'],
-      credentials:true
+      credentials: true,
+      allowedHeaders: ['Content-Type'],
+      // 'http://localhost:5173',
    }
 })
 
