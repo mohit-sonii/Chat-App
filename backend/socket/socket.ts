6744afc4d55ac0ec027c54d33ee1794bd33a7eeb
Mohit Soni <mohitsoni9731@gmail.com>
2024-09-18 16:58:51 +0530

    Trying #2 to fix socketserver issue

--- backend/socket/socket.ts
+++ backend/socket/socket.ts
@@ -26,9 +26,9 @@ export const getReceiverSocketId = (receiverId: string) => {
 const userSocketMap: socketData = {}
 
 io.on('connection', (socket) => {
-   // console.log('a user connected', socket.id) it generates random ID for that user
+   // console.log('a user connected', socket.id) 
    const userId = socket.handshake.query.userId as string | undefined
-   // console.log(userId,'this is user id after connection') it returns the Current user ID
+   console.log(userId,'this is user id after connection') 
 
    // we will store the current user Id acrross all the accounts
    if (userId && userId != 'undefined') userSocketMap[userId] = socket.id
--- dist/socket/socket.js
+++ dist/socket/socket.js
@@ -28,9 +28,9 @@ exports.getReceiverSocketId = getReceiverSocketId;
 // this will hold the current user who have been logged in 
 const userSocketMap = {};
 io.on('connection', (socket) => {
-    // console.log('a user connected', socket.id) it generates random ID for that user
+    // console.log('a user connected', socket.id) 
     const userId = socket.handshake.query.userId;
-    // console.log(userId,'this is user id after connection') it returns the Current user ID
+    console.log(userId, 'this is user id after connection');
     // we will store the current user Id acrross all the accounts
     if (userId && userId != 'undefined')
         userSocketMap[userId] = socket.id;
