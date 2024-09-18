c47fa05a3d1523454a21d935959e41272a6f6128
Mohit Soni <mohitsoni9731@gmail.com>
2024-09-18 18:39:20 +0530

    Resolving errors #3

--- dist/utils/interfaces.util.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Gender = void 0;
-var Gender;
-(function (Gender) {
-    Gender["male"] = "male";
-    Gender["female"] = "female";
-    Gender["others"] = "others";
-})(Gender || (exports.Gender = Gender = {}));
